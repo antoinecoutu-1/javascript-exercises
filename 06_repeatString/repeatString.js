682f7eb4a886ee9119f2c8ce3153bfcab7cb3e91
antoinecoutu-1 <antoine.coutu9@gmail.com>
2025-06-18 20:41:23 -0400

    Completed exercises 06 and 07

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let finalWord = ""
+    if (times < 0) {
+        finalWord = "ERROR"
+    }
+    else {
+        for (times; times > 0; times--) {
+        finalWord = finalWord.concat(word);
+        }
+    }
+    return finalWord;
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    letters = [];
+    for (i = 0; i < word.length; i++) {
+        letters.push(word[i])
+    }
+    letters.reverse();
+    wordReversed = letters.join("");
+    return wordReversed;
 };
 
 // Do not edit below this line
